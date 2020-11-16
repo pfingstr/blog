b31699a08e950acf205f9e112b33025af2eb1c22
Robert Pfingsten <pfingstr@oregonstate.edu>
2020-11-16 13:47:15 -0800

    minor fix to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import posts from '../../public/data/posts.json'
 
-export default function Navbar() {
+export default function Navbar({ posts = [] }) {
   return (
     <nav className="navbar main-navbar" role="navigation" aria-label="main navigation">
       <div className="navbar-brand">
+      
         <Link href="/">
-          <a className="navbar-item">
-          <link href="/favicon.ico" className="navbar-logo" />
+          <a className="navbar-item">   
+          <img src="/logo.png" alt="" />              
           </a>
         </Link>
       </div>
-
+ 
       <div className="navbar-menu is-active">
         <div className="navbar-start">
           <Link href="/blog">
@@ -27,4 +30,12 @@ export default function Navbar() {
       </div>
     </nav>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      posts
+    }
+  }
 }
