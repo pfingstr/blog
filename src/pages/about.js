b4d16588fb22dec2c78411fa9c30b50538784fd8
Robert Pfingsten <pfingstr@oregonstate.edu>
2020-12-03 13:28:47 -0800

    resized tile font for mobile

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,12 @@ import Layout from '../components/Layout'
 export default function About() {
   return (
     <Layout title="About">
+      <div className="grid-two-column">
+      <h1 className="title">Image</h1>
+      <p></p>
       <h1 className="title">Under Construction</h1>
+      <p></p>
+      </div>
     </Layout>
   )
 }
