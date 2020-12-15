98ac60e0d2bfc664eef5345cc1bc7328de138de8
Robert Pfingsten <pfingstr@oregonstate.edu>
2020-12-15 14:40:00 -0800

    minor index page fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,14 @@ import home from '../../public/data/about/home.json'
 export default function Home({ home }) {
   return (
     <Layout>
-      <section className="section-fill">
-        <h1 className="title has-text-centered">{home.title}</h1>
-        <h2 className="subtitle has-text-centered">{home.subtitle}</h2>
-      </section>
       
+      <section className="section"></section>
+      <section className="section">
+      <h1 className="title has-text-centered">{home.title}</h1>
+      <h2 className="subtitle has-text-centered">{home.subtitle}</h2>
+      </section>
+      <section className="section"></section>
+
     </Layout>
   )
 }
@@ -20,3 +23,4 @@ export async function getStaticProps() {
     }
   }
 }
+
